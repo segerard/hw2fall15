491e89171e3ffdf1a3c9c2f1c8713595e21614ca
segerard <sarah-gerard@uiowa.edu>
2015-09-11 18:30:54 +0000

    homework2 part5 complete

--- lib/part5.rb
+++ lib/part5.rb
@@ -2,5 +2,19 @@ class CartesianProduct
   include Enumerable
 
   # YOUR CODE HERE
+  def initialize(seq1,seq2)
+    @seq1 = seq1
+    @seq2 = seq2
+    @cart_prod = []
+    cartesian
+  end
+  
+  def cartesian
+    @seq1.each {|a| @seq2.each {|b| @cart_prod.push([a,b])} }
+  end
 
+  def each(&block)
+    @cart_prod.each &block
+  end
+  
 end
